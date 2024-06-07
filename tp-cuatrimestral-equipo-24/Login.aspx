9f4c4688535e57d5e4758392352febbd3af9d0f5
ismaoress <ismaeloreste71@gmail.com>
2024-06-06 22:00:58 -0300

    Login Diseño

---
+++ tp-cuatrimestral-equipo-24/Login.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="tp_cuatrimestral_equipo_24.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Recepcion</title>
+    <link href="Estilos2.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <section id="inicio">
+        <div>
+            <div class="presentacion">
+                <p class="bienvenida">Bienvenidos</p>
+                <h2>AL <span>RESTAURANTE</span>,BYTES Y BOCADOS</h2>
+                <p class="descripcion">
+                    El Restaurantre con la mejor critica en el mundo web 
+                     y gastronomico.
+                </p>
+                <div class="mb-3 row">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Usuario:</label>
+                    <div class="col-sm-10">
+                        <asp:TextBox ID="txtnombre" runat="server" CssClass="form-control" ></asp:TextBox>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Contraseña:</label>
+                    <div class="col-sm-10">
+                       <asp:TextBox ID="txtpassword" runat="server" CssClass="form-control" TextMode="Password"></asp:TextBox>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <asp:Button ID="txtIngresar" runat="server" CssClass="Ingresar" Text="Iniciar Sección" />
+                    <asp:Button ID="txtRegistrarse" runat="server" CssClass="Ingresar" Text="Registrarse" />
+                </div>
+            </div>
+        </div>
+    </section>
+</form>
+</body>
+</html>
---
+++ tp-cuatrimestral-equipo-24/Login.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace tp_cuatrimestral_equipo_24
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control txtnombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtnombre;
+
+        /// <summary>
+        /// Control txtpassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtpassword;
+
+        /// <summary>
+        /// Control txtIngresar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button txtIngresar;
+
+        /// <summary>
+        /// Control txtRegistrarse.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button txtRegistrarse;
+    }
+}
