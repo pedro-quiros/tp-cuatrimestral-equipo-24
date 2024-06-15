a0f3d56f84cfd4116b5955844766275f4f9d1cc7
Pedro Quiros <pedro.quiros@alumnos.frgp.utn.edu.ar>
2024-06-15 01:41:41 -0300

    Filtrado

--- tp-cuatrimestral-equipo-24/UsuarioRegistro.aspx.cs
+++ tp-cuatrimestral-equipo-24/UsuarioRegistro.aspx.cs
@@ -3,63 +3,61 @@ using Negocio;
 using System;
 using System.Collections.Generic;
 
-
 namespace tp_cuatrimestral_equipo_24
 {
     public partial class UsuarioRegistro : System.Web.UI.Page
     {
         List<Usuario> listaUsuario;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
                 UsuarioGestion usuarioGest = new UsuarioGestion();
                 List<Usuario> listaUsu = new List<Usuario>();
-                Usuario usu = new Usuario();
 
                 if (Session["Listado"] != null)
                 {
                     listaUsu = (List<Usuario>)Session["Listado"];
                 }
+                else
+                {
+                    // Cargar la lista de usuarios si no está en la sesión
+                    listaUsu = usuarioGest.ListarConSpUsuario();
+                    Session["Listado"] = listaUsu;
+                }
+
                 try
                 {
                     if (Request.QueryString["IdUsuario"] != null)
                     {
                         int id = Convert.ToInt32(Request.QueryString["IdUsuario"]);
-                        foreach (Usuario item in listaUsu)
+                        Usuario usu = listaUsu.Find(u => u.Id == id);
+
+                        if (usu != null)
                         {
-                            if (id == item.Id)
-                            {
-                                usu = item;
-                            }
+                            txtId.Value = usu.Id.ToString();
+                            txtUsuario.Value = usu.NombreUsuario;
                         }
-                        // txtId.Value = usu.Id.ToString();
-                        txtId.Value = usu.Id.ToString();
-                        txtUsuario.Value = usu.NombreUsuario;
-
-
-
-
+                        else
+                        {
+                            Response.Write("<script>alert('Usuario no encontrado.');</script>");
+                        }
                     }
-
                 }
-
-
                 catch (Exception ex)
                 {
                     Response.Write($"<script>alert('Error: {ex.Message}');</script>");
                 }
             }
-
         }
-
 
         protected void btnInactivo_Click(object sender, EventArgs e)
         {
             try
             {
                 UsuarioGestion usuario = new UsuarioGestion();
-                //usuario.BajaLogica(int.Parse(txtId));
+                // usuario.BajaLogica(int.Parse(txtId.Value));
                 Response.Redirect("Home.aspx");
             }
             catch (Exception ex)
