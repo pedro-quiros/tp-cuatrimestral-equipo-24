c98a6b8b91fd1f384e6aa0390dd9625121876354
Pedro Quiros <pedro.quiros@alumnos.frgp.utn.edu.ar>
2024-06-29 04:39:59 -0300

    ok

--- Dominio/ItemPedido.cs
+++ Dominio/ItemPedido.cs
@@ -1,20 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dominio
 {
     public class ItemPedido
     {
         public int IdItemPedido { get; set; }
-        public Pedido Pedido { get; set; }
-        public Insumo Insumo { get; set; }
+        public Pedido Pedido { get; set; } // Relación con la clase Pedido
+        public Insumo Insumo { get; set; } // Relación con la clase Insumo
         public int Cantidad { get; set; }
-        public float PrecioUnitario { get; set; }
+        public decimal PrecioUnitario { get; set; } // Actualizamos de float a decimal para mayor precisión en precios
 
-        public ItemPedido() 
+        public ItemPedido()
         {
             IdItemPedido = 0;
             Pedido = new Pedido();
@@ -23,7 +19,7 @@ namespace Dominio
             PrecioUnitario = 0;
         }
 
-        public ItemPedido (int idItemPedido, Pedido pedido, Insumo insumo, int cantidad, float precioUnitario)
+        public ItemPedido(int idItemPedido, Pedido pedido, Insumo insumo, int cantidad, decimal precioUnitario)
         {
             IdItemPedido = idItemPedido;
             Pedido = pedido;
--- Dominio/Pedido.cs
+++ Dominio/Pedido.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dominio
 {
@@ -11,18 +8,28 @@ namespace Dominio
         public int IdPedido { get; set; }
         public DateTime FechaHoraGenerado { get; set; }
         public bool Estado { get; set; }
+        public decimal Total { get; set; } // Actualizamos de float a decimal para mayor precisión en precios
+        public Mesas Mesa { get; set; } // Relación con la clase Mesas
+        public List<ItemPedido> ItemsPedido { get; set; } // Relación con la clase ItemPedido
 
         public Pedido()
         {
             IdPedido = 0;
             FechaHoraGenerado = DateTime.Now;
             Estado = true;
+            Total = 0;
+            Mesa = new Mesas();
+            ItemsPedido = new List<ItemPedido>();
         }
 
-        public Pedido(int idPedido,bool estado)
+        public Pedido(int idPedido, DateTime fechaHoraGenerado, bool estado, decimal total, Mesas mesa)
         {
             IdPedido = idPedido;
+            FechaHoraGenerado = fechaHoraGenerado;
             Estado = estado;
+            Total = total;
+            Mesa = mesa;
+            ItemsPedido = new List<ItemPedido>();
         }
     }
 }
