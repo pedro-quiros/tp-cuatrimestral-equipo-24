7e60f8e3e97c0d03b39388e5d54ab95e88a712ca
ismaoress <ismaeloreste71@gmail.com>
2024-07-01 23:20:27 -0300

    Permiso gerente clase

---
+++ Negocio/PermisoHelper.cs
@@ -0,0 +1,32 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.SessionState;
+
+namespace Negocio
+{
+    public static class PermisoHelper
+    {
+        public static bool EsGerente(HttpSessionState session)
+        {
+            if (session["UsuarioSeleccionado"] != null)
+            {
+                Usuario usuario = (Usuario)session["UsuarioSeleccionado"];
+                return usuario.tipoUsuario == TipoUsuario.GERENTE;
+            }
+            return false;
+        }
+        public static void VerificarPermisoGerente(HttpSessionState session)
+        {
+            if (!EsGerente(session))
+            {
+                HttpContext.Current.Response.Redirect("Error.aspx");
+            }
+        }
+
+    }
+}
