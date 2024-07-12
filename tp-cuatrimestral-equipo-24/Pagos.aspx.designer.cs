e9549c5ea3d544258ad90a92782d437cdf8466c1
ismaoress <ismaeloreste71@gmail.com>
2024-07-12 20:41:53 -0300

    ++

--- tp-cuatrimestral-equipo-24/Pagos.aspx.designer.cs
+++ tp-cuatrimestral-equipo-24/Pagos.aspx.designer.cs
@@ -42,6 +42,24 @@ namespace tp_cuatrimestral_equipo_24
         protected global::System.Web.UI.WebControls.Button btnConfirmar;
 
         /// <summary>
+        /// Control divtext.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divtext;
+
+        /// <summary>
+        /// Control htext.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl htext;
+
+        /// <summary>
         /// Control lblMesa.
         /// </summary>
         /// <remarks>
