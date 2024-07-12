5099374db3727f49c07bfd36a5a3d593c525c019
ismaoress <ismaeloreste71@gmail.com>
2024-07-12 20:33:48 -0300

    ++++++++

---
+++ tp-cuatrimestral-equipo-24/Pagos.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace tp_cuatrimestral_equipo_24
+{
+    public partial class Pagos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            
+            lblMesa.Visible = true;
+            lblmesero.Visible = true;
+            lblFecha.Visible = true;
+            lbltotal.Visible = true;
+            lblTipo.Visible = true;
+            lblconsumicion.Visible = true;
+            
+            txtMesa.Visible = true; 
+            txtMesero.Visible = true; 
+            Txtfecha.Visible = true; 
+            txtTotal.Visible = true; 
+            txtTipo.Visible = true; 
+            txtConsumicion.Visible = true; 
+        }
+    }
+}
